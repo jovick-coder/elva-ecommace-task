fab8e948abf70e3d2d4e4df6160a8a63198cf261
Jovick-coder <victorjosiahm3@gmail.com>
2022-08-03 18:42:26 +0100

    add review image

--- src/components/stars/Stars.jsx
+++ src/components/stars/Stars.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./StarComponent.css";
 import { FaStar } from "react-icons/fa";
+import profilePix from "../../assets/profile-pix.jpg";
 
 function StarComponent({ rate }) {
   return (
@@ -20,7 +21,9 @@ export function StarImageComponent({ rate }) {
   return (
     <>
       {[...Array(rate)].map((e, i) => (
-        <div className="starImage" key={i}></div>
+        <div className="starImage" key={i}>
+          <img src={profilePix} alt="" />
+        </div>
       ))}
       <div className="starImage">48</div>
     </>
