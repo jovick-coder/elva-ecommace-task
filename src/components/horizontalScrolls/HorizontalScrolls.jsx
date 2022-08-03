b8872a1e50f3b7ec95ca9d4d7d7e15bdd3fc3ac2
Jovick-coder <victorjosiahm3@gmail.com>
2022-08-03 18:42:15 +0100

    scroll component

---
+++ src/components/horizontalScrolls/HorizontalScrolls.jsx
@@ -0,0 +1,140 @@
+import React from "react";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import FoodCardComponent from "../foodCard/FoodCard";
+
+const getItems = () =>
+  Array(20)
+    .fill(0)
+    .map((_, ind) => ({ id: `element-${ind}` }));
+
+function HorizontalScrolls() {
+  const [items, setItems] = React.useState(getItems);
+  const [selected, setSelected] = React.useState([]);
+  const [position, setPosition] = React.useState(0);
+
+  const isItemSelected = (id) => !!selected.find((el) => el === id);
+
+  const handleClick =
+    (id) =>
+    ({ getItemById, scrollToItem }) => {
+      // console.log(getItemById);
+      // console.log(scrollToItem);
+      const itemSelected = isItemSelected(id);
+
+      console.log(itemSelected);
+
+      setSelected((currentSelected) =>
+        itemSelected
+          ? currentSelected.filter((el) => el !== id)
+          : currentSelected.concat(id)
+      );
+    };
+
+  return (
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+      {items.map(({ id }) => (
+        <Card
+          itemId={id} // NOTE: itemId is required for track items
+          title={id}
+          key={id}
+          onClick={handleClick(id)}
+          selected={isItemSelected(id)}
+        />
+      ))}
+    </ScrollMenu>
+  );
+}
+
+function LeftArrow() {
+  const { isFirstItemVisible, scrollPrev } =
+    React.useContext(VisibilityContext);
+
+  return (
+    <button disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
+      Left
+    </button>
+  );
+}
+
+function RightArrow() {
+  const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
+
+  return (
+    <button disabled={isLastItemVisible} onClick={() => scrollNext()}>
+      Right
+    </button>
+  );
+}
+
+function Card({ onClick, selected, title, itemId }) {
+  const visibility = React.useContext(VisibilityContext);
+
+  return (
+    <div
+      onClick={() => onClick(visibility)}
+      style={{
+        width: "160px",
+      }}
+      tabIndex={0}
+    >
+      <div className="card">
+        <div>{title}</div>
+        <div>visible: {JSON.stringify(!!visibility.isItemVisible(itemId))}</div>
+        <div>selected: {JSON.stringify(!!selected)}</div>
+      </div>
+      <div
+        style={{
+          height: "200px",
+        }}
+      />
+    </div>
+  );
+}
+
+export default HorizontalScrolls;
+
+export function HorizontalScrollComponent({ foodObject }) {
+  const [items, setItems] = React.useState([]);
+  const [startIndex, setStartIndex] = React.useState(0);
+  const [endIndex, setEndIndex] = React.useState(2);
+  const [visibleItems, setVisibleItems] = React.useState([]);
+  const [visibleItemsIndex, setVisibleItemsIndex] = React.useState([0, 1, 2]);
+
+  React.useEffect(() => {
+    setItems(foodObject.meals);
+  }, []);
+  function prevItemFunction() {}
+
+  function nextItemFunction() {}
+  function setVisibleItemFunction() {
+    // let
+    // setVisibleItems([])
+  }
+
+  return (
+    <>
+      <div className="scroll-div w-100">
+        {items.map((item, index) => {
+          const { strMeal, strMealThumb, idMeal } = item;
+
+          if (index <= startIndex && index >= endIndex) {
+            // if (index === startIndex) {
+            console.log(item);
+            return (
+              <>
+                <FoodCardComponent
+                  img={strMealThumb}
+                  name={strMeal}
+                  price={"200"}
+                  id={idMeal}
+                />
+              </>
+            );
+          }
+        })}
+      </div>
+      <button onClick={() => prevItemFunction()}> {"<<"} </button>
+      <button onClick={() => nextItemFunction()}> {">>"}</button>
+    </>
+  );
+}
