bf912ae89850ef81820f2616579daaf60c687ac7
Jovick-coder <victorjosiahm3@gmail.com>
2022-08-06 11:07:28 +0100

    comment out

--- src/components/horizontalScrolls/HorizontalScrolls.jsx
+++ src/components/horizontalScrolls/HorizontalScrolls.jsx
@@ -7,96 +7,96 @@ import {
   FaDotCircle,
 } from "react-icons/fa";
 
-const getItems = () =>
-  Array(20)
-    .fill(0)
-    .map((_, ind) => ({ id: `element-${ind}` }));
+// const getItems = () =>
+//   Array(20)
+//     .fill(0)
+//     .map((_, ind) => ({ id: `element-${ind}` }));
 
-function HorizontalScrolls() {
-  const [items, setItems] = React.useState(getItems);
-  const [selected, setSelected] = React.useState([]);
-  const [position, setPosition] = React.useState(0);
+// function HorizontalScrolls() {
+//   const [items, setItems] = React.useState(getItems);
+//   const [selected, setSelected] = React.useState([]);
+//   const [position, setPosition] = React.useState(0);
 
-  const isItemSelected = (id) => !!selected.find((el) => el === id);
+//   const isItemSelected = (id) => !!selected.find((el) => el === id);
 
-  const handleClick =
-    (id) =>
-    ({ getItemById, scrollToItem }) => {
-      // console.log(getItemById);
-      // console.log(scrollToItem);
-      const itemSelected = isItemSelected(id);
+//   const handleClick =
+//     (id) =>
+//     ({ getItemById, scrollToItem }) => {
+//       // console.log(getItemById);
+//       // console.log(scrollToItem);
+//       const itemSelected = isItemSelected(id);
 
-      console.log(itemSelected);
+//       console.log(itemSelected);
 
-      setSelected((currentSelected) =>
-        itemSelected
-          ? currentSelected.filter((el) => el !== id)
-          : currentSelected.concat(id)
-      );
-    };
+//       setSelected((currentSelected) =>
+//         itemSelected
+//           ? currentSelected.filter((el) => el !== id)
+//           : currentSelected.concat(id)
+//       );
+//     };
 
-  return (
-    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-      {items.map(({ id }) => (
-        <Card
-          itemId={id} // NOTE: itemId is required for track items
-          title={id}
-          key={id}
-          onClick={handleClick(id)}
-          selected={isItemSelected(id)}
-        />
-      ))}
-    </ScrollMenu>
-  );
-}
+//   return (
+//     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+//       {items.map(({ id }) => (
+//         <Card
+//           itemId={id} // NOTE: itemId is required for track items
+//           title={id}
+//           key={id}
+//           onClick={handleClick(id)}
+//           selected={isItemSelected(id)}
+//         />
+//       ))}
+//     </ScrollMenu>
+//   );
+// }
 
-function LeftArrow() {
-  const { isFirstItemVisible, scrollPrev } =
-    React.useContext(VisibilityContext);
+// function LeftArrow() {
+//   const { isFirstItemVisible, scrollPrev } =
+//     React.useContext(VisibilityContext);
 
-  return (
-    <button disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
-      Left
-    </button>
-  );
-}
+//   return (
+//     <button disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
+//       Left
+//     </button>
+//   );
+// }
 
-function RightArrow() {
-  const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
+// function RightArrow() {
+//   const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
 
-  return (
-    <button disabled={isLastItemVisible} onClick={() => scrollNext()}>
-      Right
-    </button>
-  );
-}
+//   return (
+//     <button disabled={isLastItemVisible} onClick={() => scrollNext()}>
+//       Right
+//     </button>
+//   );
+// }
 
-function Card({ onClick, selected, title, itemId }) {
-  const visibility = React.useContext(VisibilityContext);
+// function Card({ onClick, selected, title, itemId }) {
+//   const visibility = React.useContext(VisibilityContext);
 
-  return (
-    <div
-      onClick={() => onClick(visibility)}
-      style={{
-        width: "160px",
-      }}
-      tabIndex={0}
-    >
-      <div className="card">
-        <div>{title}</div>
-        <div>visible: {JSON.stringify(!!visibility.isItemVisible(itemId))}</div>
-        <div>selected: {JSON.stringify(!!selected)}</div>
-      </div>
-      <div
-        style={{
-          height: "200px",
-        }}
-      />
-    </div>
-  );
-}
+//   return (
+//     <div
+//       onClick={() => onClick(visibility)}
+//       style={{
+//         width: "160px",
+//       }}
+//       tabIndex={0}
+//     >
+//       <div className="card">
+//         <div>{title}</div>
+//         <div>visible: {JSON.stringify(!!visibility.isItemVisible(itemId))}</div>
+//         <div>selected: {JSON.stringify(!!selected)}</div>
+//       </div>
+//       <div
+//         style={{
+//           height: "200px",
+//         }}
+//       />
+//     </div>
+//   );
+// }
 
-export default HorizontalScrolls;
+// export default HorizontalScrolls;
 
 export function HorizontalScrollComponent() {
   const [items, setItems] = useState({
